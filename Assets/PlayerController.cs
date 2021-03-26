4060e15f4c5cb20658bfa73bae1b750094ae7ed6
vinhphuctadang <vinhphuctadang@gmail.com>
2021-03-26 23:25:06 +0700

    update getting pose from API

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.Networking;
 
 public class PlayerController : MonoBehaviour
 {
-    
-    Animator animator;
+    public static string actionUri = "http://192.168.0.107:5000/pose";
+    Animator animator; 
+    public string action = "";
+    public IEnumerator getPoseRequest;
+
+    [Serializable]
+    public class PoseResponse
+    {
+        public string code;
+        public string action;
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
         animator = gameObject.GetComponent<Animator>();
+        getPoseRequest = GetPose();
+        StartCoroutine(getPoseRequest);
     }
 
     void OnCollisionEnter(Collision collision)
@@ -20,22 +35,39 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("up"))
-        {
-            Debug.Log("Walking pressed");
+        
+        if (action == "walking") {
             animator.SetFloat("MoveSpeed", 2.0f);
         }
-        else if (Input.GetKeyUp("up")){
-            Debug.Log("Walking released");
-            animator.SetFloat("MoveSpeed", 0.0f);
-        }
-        else if (Input.GetKeyDown("w")){
-            Debug.Log("Running pressed");
+        else if (action == "running"){
             animator.SetFloat("MoveSpeed", 6f);
         }
-        else if (Input.GetKeyUp("w")){
-            Debug.Log("Running released");
+        else if (action == "idle"){   
             animator.SetFloat("MoveSpeed", 0f);
+        }
+    }
+
+    IEnumerator GetPose()
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(actionUri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError("Error in request:" + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    var text = webRequest.downloadHandler.text;
+                    var response = JsonUtility.FromJson<PoseResponse>(text);
+                    this.action = response.action;
+                    Debug.Log("New action updated: " + action);
+                    break;
+            }
         }
     }
 }
